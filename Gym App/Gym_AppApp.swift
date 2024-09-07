265346d6e0bd60850fcb0e0788b237444dbd2a9b
Panos Daskalopoulos <alexdask1@icloud.com>
2024-09-07 15:55:16 +0300

    Initial Commit

---
+++ Gym App/Gym_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_AppApp.swift
+//  Gym App
+//
+//  Created by Panos Daskalopoulos on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
