34981b2ab842b732f696b32e0d00df77da2c81ce
Panos Daskalopoulos <alexdask1@icloud.com>
2024-09-08 18:45:53 +0300

    Added A new page view

--- Gym App/AppointmentView.swift
+++ Gym App/AppointmentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct AppointmentView: View {
+@Environment(\.colorScheme) var colorScheme
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                AppointmentCardView(AppointmentCard: AppointmentCard(category: "person"))
+               
+            }
+            .toolbar {
+                // Bottom bar
+                ToolbarItem(placement: .bottomBar) {
+                    HStack {
+                        SButtonView(SButton: SButton(symbol: "person"))
+                        SButtonView(SButton: SButton(symbol: "calendar"))
+                        SButtonView(SButton: SButton(symbol: "note"))
+                        SButtonView(SButton: SButton(symbol: "book"))
+                    }
+                    .scaleEffect(1.8)
+                }
+            }
+            .foregroundColor(Color("MainColor"))
+        }
     }
 }
+
+struct AppointmentCard {
+    var category: String
+}
+
+struct AppointmentCardView: View {
+    let AppointmentCard : AppointmentCard
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+
 
 #Preview {
     AppointmentView()
