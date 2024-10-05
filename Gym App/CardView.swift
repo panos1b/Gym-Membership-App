36731dc79c209a8e485fd34a102c538f7272ece3
Panos Daskalopoulos <alexdask1@icloud.com>
2024-10-05 20:30:56 +0300

    Added Cards to the Appointment View

---
+++ Gym App/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Gym App
+//
+//  Created by Panos Daskalopoulos on 5/10/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
